8e11b93a999c9313b9c40918e2c1dfb5d3495660
Muna Mahadi <munammahadi@gmail.com>
2024-01-28 19:34:21 +0200

    bmi calculator

---
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.bmi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bmi.ui.theme.BmiTheme
+import androidx.compose.ui.unit.*
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BmiTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Bmi()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Bmi() {
+    var heightInput: String by remember { mutableStateOf("") }
+    var weightInput: String by remember { mutableStateOf("") }
+    val height = heightInput.toFloatOrNull() ?: 0.0f
+    val weight = weightInput.toIntOrNull() ?: 0
+    val bmi = if (weight > 0 && height > 0) weight / (height * height) else 0.0
+    Column() {
+        Text(
+            text = "Body mass index",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.primary,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 16.dp),
+        )
+        OutlinedTextField(
+            value = heightInput,
+            onValueChange = { heightInput = it.replace(',', '.') },
+            label = {Text(stringResource(R.string.height))},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = weightInput,
+            onValueChange = { weightInput = it.replace(',', '.') },
+            label = {Text(stringResource(R.string.weight))},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(text = stringResource(R.string.result, String.format("%.2f", bmi).replace(',', '.')))
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BmiTheme {
+        Bmi()
+    }
+}
